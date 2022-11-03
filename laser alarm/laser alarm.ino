78e39978329a4f411c762f13ef3dc86a4fd0861a
Rachniotov <71971848+Rachniotov@users.noreply.github.com>
2022-11-03 21:30:47 +0530

    update laser alarm

--- laser alarm/laser alarm.ino
+++ laser alarm/laser alarm.ino
@@ -1,4 +1,11 @@
-const int ldr = A0;
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+
+const char* ssid = "Rachit jio";
+const char* pass = "wifi@9999";
+
+const int ldr = 12;
 const int buz = 14;
 
 void setup() {
@@ -6,13 +13,33 @@ void setup() {
   pinMode(buz, OUTPUT);
 
   Serial.begin(9600);
+
+  WiFi.begin(ssid, pass);
+
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println("Connected!");
 }
 
 void loop() {
-  int data = analogRead(ldr);
+  int data = digitalRead(ldr);
   Serial.println(data);
 
-  if (data < 100) {
-    tone(buz, 1000, 500);
+  if (data == 0) {
+    // tone(buz, 1000, 500);
+
+    if (WiFi.status() == WL_CONNECTED) {
+      HTTPClient http;
+      WiFiClient client;
+
+      http.begin(client, "http://192.168.29.148:5999/door-intruder");
+      int resCode = http.GET();
+
+      http.end();
+    }
+  delay(500);
   }
 }
